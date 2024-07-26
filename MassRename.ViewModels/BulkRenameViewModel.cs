5c8daa8032918cfb3e2dec17949f9a8e4975a290
Aurumaker72 <armkdev72@gmail.com>
2024-07-26 23:02:16 +0200

    Add SpacedFused suggestion

--- MassRename.ViewModels/BulkRenameViewModel.cs
+++ MassRename.ViewModels/BulkRenameViewModel.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MassRename.Services;
@@ -10,16 +11,16 @@ public partial class BulkRenameViewModel : ObservableObject
     /// The current index into the rename queue. Never exceeds <see cref="FileCount"/>.
     /// </summary>
     public int CurrentIndex { get; private set; }
-    
+
     /// <summary>
     /// The size of the rename queue.
     /// </summary>
     public int FileCount => _files.Count;
-    
+
     /// <summary>
     /// The current name in the rename queue.
     /// </summary>
-    public string CurrentFile => _files[CurrentIndex];
+    public string CurrentFile => Path.GetFileNameWithoutExtension(_files[CurrentIndex]);
 
     /// <summary>
     /// The name suggestions for <see cref="CurrentFile"/>.
@@ -28,7 +29,7 @@ public partial class BulkRenameViewModel : ObservableObject
 
     private readonly IReadOnlyList<string> _files;
     private readonly INavigationService _navigationService;
-    
+
     public BulkRenameViewModel(IReadOnlyList<string> files, INavigationService navigationService)
     {
         _files = files;
@@ -63,20 +64,59 @@ public partial class BulkRenameViewModel : ObservableObject
         }
 
         CurrentIndex += entries;
-        NameSuggestions = ComputeSuggestions(CurrentFile);
+        NameSuggestions = ComputeSuggestions(CurrentFile).ToArray();
 
         OnPropertyChanged(nameof(CurrentIndex));
         OnPropertyChanged(nameof(CurrentFile));
         OnPropertyChanged(nameof(NameSuggestions));
     }
 
+    [GeneratedRegex(@"\s{2,}")]
+    private static partial Regex WhitespaceCompressorRegex();
+
     /// <summary>
     /// Computes name suggestions for the specified file name
     /// </summary>
     /// <param name="name">The file stem</param>
     /// <returns>An array of name suggestions</returns>
-    private string[] ComputeSuggestions(string name)
+    private List<string> ComputeSuggestions(string name)
     {
-        return [name, "jordan"];
+        List<string> suggestions = [];
+
+        suggestions.AddRange(ComputeSuggestion_SpacedFused(name));
+
+        suggestions.Add(name);
+        return suggestions;
+    }
+
+    private List<string> ComputeSuggestion_SpacedFused(string name)
+    {
+        // 1. Compress repeated spcaes before splitting
+        var computed = WhitespaceCompressorRegex().Replace(name, "");
+
+        // 2. Split the string by spaces
+        var parts = computed.Split(' ');
+
+        // 3. If there are 2 or less segments, we bail
+        if (parts.Length < 2)
+        {
+            return [];
+        }
+
+        // 4. Exactly two segments, we can keep it
+        if (parts.Length == 2)
+        {
+            return [$"{parts[0]} - {parts[1]}"];
+        }
+
+        // 5. More than 2 segments, fuse the ones following the first one in-place
+        // Artist - Track - Something - Else
+        // vvvv
+        // Artist - Track (Something Else)
+        
+        var tail = parts[1..];
+        var fused = string.Join(" ", tail);
+        
+        return [$"{parts[0]} - ({fused})"];
     }
 }
