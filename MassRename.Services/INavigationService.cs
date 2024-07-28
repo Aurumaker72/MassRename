8804a6a711617702f3675a8935d0cfab9e0763d6
Aurumaker72 <armkdev72@gmail.com>
2024-07-28 09:33:25 +0200

    Remove wrong todo

--- MassRename.Services/INavigationService.cs
+++ MassRename.Services/INavigationService.cs
@@ -7,7 +7,6 @@ public interface INavigationService
 {
     /// <summary>
     /// Switches the navigation context to a ViewModel instance
-    /// TODO: Take in a nullable type, as the View should be responsible for VM creation
     /// </summary>
     /// <param name="viewModel">The ViewModel backing the next view, or null if the default should be chosen</param>
     void SwitchTo(object? viewModel);
